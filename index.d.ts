949330182a1651e233d36f2766756859512e699b
Ben Durrant <ben.j.durrant@gmail.com>
2025-08-14 09:23:44 +0100

    improve type declaration files (#31)

---
+++ index.d.ts
@@ -0,0 +1,65 @@
+import {
+  Observable as _Observable,
+  ObservableEventListenerOptions as _ObservableEventListenerOptions,
+} from "./observable";
+
+// the "type" here is important, we want to re-export all types without the runtime exports
+export type * from "./observable";
+
+declare global {
+  type ObservableEventListenerOptions = _ObservableEventListenerOptions;
+
+  type Observable<T = any> = _Observable<T>;
+  var Observable: typeof _Observable;
+
+  interface EventTarget {
+    when(
+      type: string,
+      options?: ObservableEventListenerOptions
+    ): Observable<Event>;
+  }
+
+  interface Document {
+    when<T extends keyof DocumentEventMap>(
+      event: T,
+      options?: ObservableEventListenerOptions
+    ): Observable<DocumentEventMap[T]>;
+    when(
+      type: string,
+      options?: ObservableEventListenerOptions
+    ): Observable<Event>;
+  }
+
+  interface HTMLElement {
+    when<T extends keyof HTMLElementEventMap>(
+      event: T,
+      options?: ObservableEventListenerOptions
+    ): Observable<HTMLElementEventMap[T]>;
+    when(
+      type: string,
+      options?: ObservableEventListenerOptions
+    ): Observable<Event>;
+  }
+
+  interface Element {
+    when<T extends keyof ElementEventMap>(
+      event: T,
+      options?: ObservableEventListenerOptions
+    ): Observable<HTMLElementEventMap[T]>;
+    when(
+      type: string,
+      options?: ObservableEventListenerOptions
+    ): Observable<Event>;
+  }
+
+  interface Window {
+    when<T extends keyof HTMLElementEventMap>(
+      event: T,
+      options?: ObservableEventListenerOptions
+    ): Observable<HTMLElementEventMap[T]>;
+    when(
+      type: string,
+      options?: ObservableEventListenerOptions
+    ): Observable<Event>;
+  }
+}
--- observable.d.ts
+++ observable.d.ts
@@ -1,2 +1,99 @@
-export * from './index-fn.d.ts';
+export interface Subscriber<T = any> {
+  next(value: T): void;
+  error(error: any): void;
+  complete(): void;
+  addTeardown(teardown: () => void): void;
+  readonly active: boolean;
+  readonly signal: AbortSignal;
+}
 
+export type SubscribeCallback<T = any> = (subscriber: Subscriber<T>) => void;
+export type ObservableSubscriptionCallback<T = any> = (value: T) => void;
+
+export interface SubscriptionObserver<T = any> {
+  next?: ObservableSubscriptionCallback<T>;
+  error?: ObservableSubscriptionCallback;
+  complete?: () => void;
+}
+
+export type ObservableInspectorAbortHandler = (value: any) => void;
+
+export interface ObservableInspector<T = any> {
+  next?: ObservableSubscriptionCallback<T>;
+  error?: ObservableSubscriptionCallback;
+  complete?: () => void;
+  subscribe?: () => void;
+  abort?: ObservableInspectorAbortHandler;
+}
+
+export type ObserverUnion<T = any> =
+  | ObservableSubscriptionCallback<T>
+  | SubscriptionObserver<T>;
+export type ObservableInspectorUnion<T = any> =
+  | ObservableSubscriptionCallback<T>
+  | ObservableInspector<T>;
+
+export interface SubscribeOptions {
+  signal?: AbortSignal;
+}
+
+export type TypePredicate<T = any, U extends T = T> = (
+  value: T,
+  index: number
+) => value is U;
+export type Predicate<T = any> = (value: T, index: number) => unknown;
+export type Reducer<T = any, R = any> = (
+  accumulator: R,
+  currentValue: T,
+  index: number
+) => R;
+export type Mapper<T = any, R = any> = (value: T, index: number) => R;
+export type Visitor<T = any> = (value: T, index: number) => void;
+export type CatchCallback<R = any> = (value: any) => Observable<R> | R;
+
+export type ObservableInput<T> =
+  | Observable<T>
+  | AsyncIterable<T>
+  | Iterable<T>
+  | PromiseLike<T>;
+
+export class Observable<T = any> {
+  constructor(callback: SubscribeCallback<T>);
+  subscribe(observer?: ObserverUnion<T>, options?: SubscribeOptions): void;
+  static from<T>(value: ObservableInput<T>): Observable<T>;
+  takeUntil(value: ObservableInput<any>): Observable<T>;
+  map<R>(mapper: Mapper<T, R>): Observable<R>;
+  filter<U extends T>(predicate: TypePredicate<T, U>): Observable<U>;
+  filter(predicate: Predicate<T>): Observable<T>;
+  take(amount: number): Observable<T>;
+  drop(amount: number): Observable<T>;
+  flatMap<R>(mapper: Mapper<T, ObservableInput<R>>): Observable<R>;
+  switchMap<R>(mapper: Mapper<T, ObservableInput<R>>): Observable<R>;
+  inspect(inspectorUnion?: ObservableInspectorUnion<T>): Observable<T>;
+  catch(callback: CatchCallback): Observable<T>;
+  finally(callback: () => void): Observable<T>;
+  toArray(options?: SubscribeOptions): Promise<T[]>;
+  forEach(callback: Visitor<T>, options?: SubscribeOptions): Promise<void>;
+  every(predicate: Predicate<T>, options?: SubscribeOptions): Promise<boolean>;
+  first(options?: SubscribeOptions): Promise<T>;
+  last(options?: SubscribeOptions): Promise<T>;
+  find(
+    predicate: Predicate<T>,
+    options?: SubscribeOptions
+  ): Promise<T | undefined>;
+  some(predicate: Predicate<T>, options?: SubscribeOptions): Promise<boolean>;
+  reduce<R>(
+    reducer: Reducer<T, R>,
+    initialValue?: R,
+    options?: SubscribeOptions
+  ): Promise<R>;
+}
+
+export interface ObservableEventListenerOptions {
+  capture?: boolean;
+  passive?: boolean;
+}
+
+export declare function isSupported(): boolean;
+export declare function isPolyfilled(): boolean;
+export declare function apply(): void;
