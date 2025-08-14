6cdc1e6467fdc0b33e79d8eb467c8b3dbf7ceda5
Ben Durrant <ben.durrant@marketdojo.com>
2025-08-14 10:33:09 +0100

    add type test setup

---
+++ test/types/globals.test-d.ts
@@ -0,0 +1,47 @@
+import "observable-polyfill";
+import * as observable from "observable-polyfill";
+import { describe, it, expectTypeOf } from "vitest";
+
+describe("Globals", () => {
+  it("registers global Observable class and type", () => {
+    expectTypeOf(Observable).toEqualTypeOf<typeof observable.Observable>();
+    expectTypeOf<Observable>().toEqualTypeOf<observable.Observable>();
+  });
+  it("registers global ObservableEventListenerOptions type", () => {
+    expectTypeOf<ObservableEventListenerOptions>().toEqualTypeOf<observable.ObservableEventListenerOptions>();
+  });
+  it("augments EventTarget with when()", () => {
+    expectTypeOf<EventTarget>().toHaveProperty("when");
+    const target = new EventTarget();
+    expectTypeOf(target.when("foo")).toEqualTypeOf<Observable<Event>>();
+  });
+  it("augments Document with when(), including specific types", () => {
+    expectTypeOf<Document>().toHaveProperty("when");
+    expectTypeOf(document.when("click")).toEqualTypeOf<
+      Observable<PointerEvent>
+    >();
+    expectTypeOf(document.when("foo")).toEqualTypeOf<Observable<Event>>();
+  });
+  it("augments HTMLElement with when(), including specific types", () => {
+    expectTypeOf<HTMLElement>().toHaveProperty("when");
+    expectTypeOf(document.body.when("click")).toEqualTypeOf<
+      Observable<PointerEvent>
+    >();
+    expectTypeOf(document.body.when("foo")).toEqualTypeOf<Observable<Event>>();
+  });
+  it("augments Element with when(), including specific types", () => {
+    expectTypeOf<Element>().toHaveProperty("when");
+    const el = new Element();
+    expectTypeOf(el.when("fullscreenchange")).toEqualTypeOf<
+      Observable<Event>
+    >();
+    expectTypeOf(el.when("foo")).toEqualTypeOf<Observable<Event>>();
+  });
+  it("augments Window with when(), including specific types", () => {
+    expectTypeOf<Window>().toHaveProperty("when");
+    expectTypeOf(window.when("click")).toEqualTypeOf<
+      Observable<PointerEvent>
+    >();
+    expectTypeOf(window.when("foo")).toEqualTypeOf<Observable<Event>>();
+  });
+});
